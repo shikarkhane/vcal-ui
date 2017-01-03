f2a96495e29d1fc020059c570c6afaeca83dd16b
Nikhilm <shikarkhane@gmail.com>
2017-01-03 16:07:20 +0100

    authenticate js

---
+++ vcal-web/src/Authenticate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Authenticate extends Component {
+  render() {
+    return (
+      <div className="page-header">
+        <h1>Authenticate<small>Gomorronsol</small></h1>
+      </div>
+    );
+  }
+}
+
+export default Authenticate;
