78a04b1e0207747f7bbdde99b85465b0b9723432
nikhil shikarkhane <nikhils@spotify.com>
2017-01-05 17:36:26 +0100

    render another page example

--- vcal-web/src/BodyBottom.js
+++ vcal-web/src/BodyBottom.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Summon from './Summon';
 
 class BodyBottom extends Component {
+  handlerSummon() {
+    ReactDOM.render(
+      <Summon />,
+      document.getElementById('root')
+    );
+  }
   render() {
     return (
       <div className="page-header">
         <h1>BodyBottom<small>Gomorronsol</small></h1>
+        <button onClick={this.handlerSummon}>summon</button>
       </div>
     );
   }
