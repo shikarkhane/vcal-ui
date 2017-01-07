aa00c6a298784ce57ec90d8e34575410877b10eb
Nikhilm <shikarkhane@gmail.com>
2017-01-07 12:59:40 +0100

    ajax call example added

--- vcal-web/src/Summon.js
+++ vcal-web/src/Summon.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import reqwest from 'reqwest';
 
 class Summon extends Component {
   render() {
+
+    reqwest({
+        url: 'http://localhost:8080/sv/rule/'
+      , method: 'post'
+      , data: {standin: [1, 2, 3], workday: [4, 5, 6]}
+      , success: function (resp) {
+          console.log(resp);
+        }
+    });
+
     return (
       <div className="page-header">
         <h1>Summon<small>Gomorronsol</small></h1>
