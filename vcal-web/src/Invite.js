e6c085eeb78a010cbaff1806c14c54157ad2d751
Nikhilm <shikarkhane@gmail.com>
2017-01-08 20:33:11 +0100

    invite structure

--- vcal-web/src/Invite.js
+++ vcal-web/src/Invite.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import reqwest from 'reqwest';
 
 class Invite extends Component {
+  handleSave(){
+    var emails = 'nikhil@tinktime.com,n@g.com';
+    var group_id = 1;
+    reqwest({
+        url: 'http://localhost:8080/sv/invite/'
+      , type: 'json'
+      , method: 'post'
+      , contentType: 'application/json'
+      , data: JSON.stringify({ group_id: group_id, emails: emails })
+      , success: function (resp) {
+          console.log(resp);
+        }
+    });
+  }
   render() {
     return (
       <div className="page-header">
         <h1>Invite <small>Gomorronsol</small></h1>
+        <textarea id="textarea-invitees" placeholder="separate emails by comma sign (,) "></textarea>
+        <input type="button" value="Send invites" onClick={this.handleSave}/>
       </div>
     );
   }
