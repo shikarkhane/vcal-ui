4d29a57407baa89d7c84f5a96fe48f87867f08a7
nikhil shikarkhane <nikhils@spotify.com>
2017-01-09 11:10:56 +0100

    worksignup structure

--- vcal-web/src/WorkSignUp.js
+++ vcal-web/src/WorkSignUp.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import WorkSignUpComponent from './WorkSignUpComponent';
 
 class WorkSignUp extends Component {
   render() {
     return (
       <div className="page-header">
         <h1>WorkSignUp<small>Gomorronsol</small></h1>
+        <WorkSignUpComponent />
+        <WorkSignUpComponent />
       </div>
     );
   }
---
+++ vcal-web/src/WorkSignUpComponent.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import reqwest from 'reqwest';
+
+class WorkSignUpComponent extends Component {
+  handleSave(){
+    var group_id = 1;
+    var chosen_date = '2017-01-21';
+    var user_id = 1;
+    var is_workday = true;
+    var is_taken = true;
+
+    reqwest({
+        url: 'http://localhost:8080/sv/work-sign-up/' + group_id + "/"
+      , type: 'json'
+      , method: 'post'
+      , contentType: 'application/json'
+      , data: JSON.stringify({ chosen_date: chosen_date, user_id: user_id,
+          is_workday: is_workday, is_taken: is_taken})
+      , success: function (resp) {
+          console.log(resp);
+        }
+    });
+
+    /*
+    $.ajax({
+          contentType : 'application/json',
+          method: "GET",
+          url: "/sv/work-sign-up/" + group_id + "/"
+          // ,data: JSON.stringify({ workday_user_ids: workday_user_ids, standin_user_ids: standin_user_ids})
+        })
+          .done(function( msg ) {
+            console.log('get work sign up');
+          });
+    $.ajax({
+          contentType : 'application/json',
+          method: "GET",
+          url: "/sv/term_details/" + group_id + "/"
+        })
+          .done(function( msg ) {
+            console.log(msg);
+          });
+    */
+  }
+  render() {
+    return (
+            <div>
+              <h4>Fill {"4"} Stand-in days</h4>
+              <div className="input-group">
+                <input type="checkbox" className="form-control" data-pick-date="2017-01-13"
+                  aria-describedby="addon-pickdate-1" onChange={this.handleSave}/>
+                <span className="input-group-addon" id="addon-pickdate-1">2017-01-13</span>
+              </div>
+              <div className="input-group">
+                <input type="checkbox" className="form-control" data-pick-date="2017-01-18"
+                  aria-describedby="addon-pickdate-2" onChange={this.handleSave}/>
+                <span className="input-group-addon" id="addon-pickdate-2">2017-01-18</span>
+              </div>
+              <div className="input-group">
+                <input type="checkbox" className="form-control" data-pick-date="2017-01-21"
+                  aria-describedby="addon-pickdate-3" onChange={this.handleSave}/>
+                <span className="input-group-addon" id="addon-pickdate-3">2017-01-21</span>
+              </div>
+            </div>
+    );
+  }
+}
+
+export default WorkSignUpComponent;
