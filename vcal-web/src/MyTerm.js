5ac83259d5eec82cd36cdf355be11d0023c7a30d
nikhil shikarkhane <nikhils@spotify.com>
2017-01-12 14:16:47 +0100

    get terms

--- vcal-web/src/MyTerm.js
+++ vcal-web/src/MyTerm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import reqwest from 'reqwest';
 
 class TermElement extends Component{
   handleChooseTerm(termId, termName){
@@ -19,14 +20,36 @@ class MyTerm extends Component {
   constructor(props) {
    super(props);
    // todo: get list of active terms in the group
-   this.state = {date: new Date()};
+   this.state = {terms: []};
  }
+ componentDidMount() {
+      this.getTerms();
+  }
+  getTerms(){
+    var self = this;
+    var groupId = 1;
+     // todo: get list of terms for given group id
+     reqwest({
+         url: 'http://localhost:8080/term_details/' + groupId + '/'
+         , type: 'json'
+         , contentType: 'application/json'
+       , method: 'get'
+       , success: function (resp) {
+           var j = resp;
+           self.setState({terms: j});
+         }
+     });
+  }
   render() {
+    const tms = this.state.terms;
+    const termButtons = tms.map((tm) =>
+    <TermElement key={tm.id} termId={tm.id} termName={tm.name} />
+  );
+
     return (
       <div className="page-header">
         <h1>Term </h1>
-        <TermElement termId="1" termName="ht2016" />
-        <TermElement termId="2" termName="vt2017" />
+        {termButtons}
       </div>
     );
   }
