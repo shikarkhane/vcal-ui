6c90f8a959f97ea788e1584e7ff5f3f0a1987087
nikhil shikarkhane <nikhils@spotify.com>
2017-01-26 16:33:48 +0100

    bug fix

--- vcal-web/src/WorkSignUpComponent.js
+++ vcal-web/src/WorkSignUpComponent.js
@@ -26,13 +26,13 @@ class PickDate extends Component{
 
   render(){
     var isWorkday = this.props.isWorkday;
-    var chosenDate = new Date(this.props.chosenDate).getTime();
+    var chosenDate = this.props.chosenDate;
 
     return (
       <div className="input-group">
         <input type="checkbox" className="form-control" data-pick-date="{this.props.chosenDate}"
            onChange={this.handleSave.bind(null, isWorkday, chosenDate)}/>
-        <span className="input-group-addon" >{this.props.chosenDate}</span>
+         <span className="input-group-addon" >{getHumanDate(this.props.chosenDate)}</span>
       </div>
     );
   }
@@ -78,11 +78,11 @@ class WorkSignUpComponent extends Component {
 
     const standins = this.state.openStandin;
     const standinElements = standins.map((s) =>
-    <PickDate key={s.id} chosenDate={getHumanDate(s.standin_date)} isWorkday="0"/>
+    <PickDate key={s.id} chosenDate={s.standin_date} isWorkday="0"/>
       );
     const workdays = this.state.openWorkday;
     const workdayElements = workdays.map((s) =>
-    <PickDate key={s.id} chosenDate={getHumanDate(s.work_date)} isWorkday="1"/>
+    <PickDate key={s.id} chosenDate={s.work_date} isWorkday="1"/>
     );
     return (
             <div>
