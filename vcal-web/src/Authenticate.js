9987b16580f10754b4655746ede69e863becd0b2
Nikhilm <shikarkhane@gmail.com>
2021-10-20 20:15:03 +0200

    login working, page redirect not working

--- vcal-web/src/Authenticate.js
+++ vcal-web/src/Authenticate.js
@@ -94,6 +94,9 @@ class GoogleButton extends Component{
         hostedDomain="gomorronsol.net"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
       />
     );
   }
